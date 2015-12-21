ff418ef7430c0e4c6230d5d5123f4517e7267550
shunobaka <vesselin_minev@abv.bg>
2015-12-22 00:24:24 +0200

    Added filter for rounding decimal numbers

---
+++ Client/Negwork.Client.App/scripts/app/common/round-to-decimal-filter.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    var roundToDecimalFilter = function roundToDecimalFilter() {
+        return function (number, place) {
+            place = place || 2;
+
+            return number.toFixed(place);
+        };
+    };
+
+    angular
+        .module('negwork.filters')
+        .filter('roundToDecimal', roundToDecimalFilter);
+}());
