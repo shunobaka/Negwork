0c2c8aaca9f61d23b4222ebb04bf6db04bc590ea
shunobaka <vesselin_minev@abv.bg>
2015-12-22 00:28:56 +0200

    Improved code quality in articles-service

--- Client/Negwork.Client.App/scripts/app/articles/articles-service.js
+++ Client/Negwork.Client.App/scripts/app/articles/articles-service.js
@@ -2,21 +2,25 @@
     'use strict';
 
     var articlesService = function articlesService(data) {
+        var ARTICLES_API_URL = '/api/articles';
+        var RATINGS_API_URL = '/api/ratings';
+
         function add(article) {
-            return data.post('/api/articles', article);
+            return data.post(ARTICLES_API_URL, article);
         }
 
-        function all() {
-            return data.get('/api/articles');
+        function search(request) {
+            return data.get(ARTICLES_API_URL, request);
         }
 
-        function rate(id, rating) {
-            return data.post('/api/ratings/' + id, rating);
+        function rate(rating) {
+            return data.post(RATINGS_API_URL, rating);
         }
 
         return {
             add: add,
-            all: all
+            search: search,
+            rate: rate
         };
     };
 
