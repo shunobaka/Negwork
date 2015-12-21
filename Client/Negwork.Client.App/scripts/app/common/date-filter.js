8f1fedd255aafaab0bc1142bcc9182caca025c71
shunobaka <vesselin_minev@abv.bg>
2015-12-21 12:37:55 +0200

    Added filter for formatting date

---
+++ Client/Negwork.Client.App/scripts/app/common/date-filter.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    var dateFormatFilter = function dateFormatFilter() {
+        return function (input) {
+            var monthNames = [
+              "January", "February", "March",
+              "April", "May", "June", "July",
+              "August", "September", "October",
+              "November", "December"
+            ];
+
+            var date = new Date(input);
+            var day = date.getDate();
+            var month = monthNames[date.getMonth()];
+            var year = date.getFullYear();
+
+            var resultDate = day + ' ' + month + ' ' + year;
+
+            return resultDate;
+        }
+    };
+
+    angular
+        .module('negwork.filters')
+        .filter('dateFormat', [dateFormatFilter]);
+}());
