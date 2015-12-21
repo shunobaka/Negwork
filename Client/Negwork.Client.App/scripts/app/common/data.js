2e3be7902dbcf1d39bd29c073251c54d13d9a7c5
shunobaka <vesselin_minev@abv.bg>
2015-12-21 02:15:27 +0200

    Minor fixes and implemented data service with post and get methods

---
+++ Client/Negwork.Client.App/scripts/app/common/data.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+
+    function dataService($http, $q, apiUrl) {
+
+        function get(url, params) {
+            var defered = $q.defer();
+
+            $http
+                .get(apiUrl + url, {
+                    params: params
+                })
+                .then(function (response) {
+                    defered.resolve(response.data);
+                }, function (err) {
+                    defered.reject(err);
+                });
+
+            return defered.promise;
+        }
+
+        function post(url, data) {
+            var defered = $q.defer();
+
+            $http
+                .post(apiUrl + url, data)
+                .then(function (response) {
+                    defered.resolve(response.data);
+                }, function (err) {
+                    defered.reject(err);
+                });
+
+            return defered.promise;
+        }
+
+        return {
+            get: get,
+            post: post
+        }
+    }
+
+
+    angular
+        .module('negwork.services')
+        .factory('data', ['$http', '$q', 'apiUrl', dataService]);
+}());
