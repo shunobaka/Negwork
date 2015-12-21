0e77c16177cdb9de3c751b9eed8816c51aca0e83
shunobaka <vesselin_minev@abv.bg>
2015-12-22 00:29:10 +0200

    Implemented basic filtering

--- Client/Negwork.Client.App/scripts/app/articles/articles-controller.js
+++ Client/Negwork.Client.App/scripts/app/articles/articles-controller.js
@@ -3,26 +3,69 @@
 
     var articlesController = function articlesController(auth, notifier, articles) {
         var vm = this;
+        vm.currentPage = 1;
+        vm.DefaultProfile = 'https://diasp.eu/assets/user/default.png';
 
-        vm.isAuthenticated = auth.isAuthenticated();
+        vm.pages = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-        vm.rateArticle = function (index, rating) {
-            var id = vm.articles[index];
-            articles.rate(id, rating)
-                .then(function () {
-                    notifier.success('Article was successfully rated!');
-                }, function (err) {
-                    notifier.error(err);
-                })
-        };
-
-        articles.all()
+        articles.search()
             .then(function (response) {
-                vm.DefaultProfile = 'https://diasp.eu/assets/user/default.png';
                 vm.articles = response;
             }, function (err) {
                 notifier.error(err);
             });
+
+        vm.isAuthenticated = auth.isAuthenticated();
+
+        vm.rateArticle = function (index, rating) {
+            var id = vm.articles[index].Id;
+            var data = { ArticleId: id, Rating: rating };
+
+            articles.rate(data)
+                .then(function () {
+                    notifier.success('Article was successfully rated!');
+                }, function (err) {
+                    if (!rating) {
+                        notifier.error('No rating selected!');
+                        return;
+                    }
+
+                    var errorMsg = err.data.Message;
+                    notifier.error(errorMsg);
+                })
+        };
+
+        vm.search = function (request, page) {
+            request = request || {};
+            page = page || vm.currentPage
+            request.page = page;
+            vm.currentPage = page;
+
+            debugger;
+
+            articles.search(request)
+                .then(function (response) {
+                    vm.articles = response;
+                }, function (err) {
+                    notifier.error(err);
+                });
+        };
+
+        vm.goToNextPage = function (request) {
+            if (vm.currentPage < vm.pages.length) {
+                vm.currentPage++;
+            }
+
+            vm.search(request, vm.currentPage);
+        };
+
+        vm.goToPreviousPage = function (request) {
+            if (vm.currentPage > 1) {
+                vm.currentPage--;
+            }
+
+            vm.search(request, vm.currentPage);
+        };
     }
 
     angular
