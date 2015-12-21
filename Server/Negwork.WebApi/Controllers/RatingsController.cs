821390ee1d40fb7fb071b58ce813760deee12a2e
shunobaka <vesselin_minev@abv.bg>
2015-12-22 00:28:08 +0200

    Implemented controller for rating articles

--- Server/Negwork.WebApi/Controllers/RatingsController.cs
+++ Server/Negwork.WebApi/Controllers/RatingsController.cs
@@ -1,8 +1,9 @@
 ﻿namespace Negwork.WebApi.Controllers
 {
     using Microsoft.AspNet.Identity;
+    using Models.RatingModels;
+    using Services.Data;
     using Services.Data.Contracts;
-    using System;
     using System.Web.Http;
 
     public class RatingsController : ApiController
@@ -15,13 +16,28 @@
         }
 
         [Authorize]
-        public IHttpActionResult Post(int id, int rating)
+        public IHttpActionResult Post(RatingCreationModel model)
         {
-            var ratedArticle = data.RateArticle(this.User.Identity.GetUserId(), id, rating);
-
-            if (ratedArticle == null)
+            if (!this.ModelState.IsValid)
             {
-                return this.BadRequest("There was a problem rating the article.");
+                return this.BadRequest(this.ModelState);
+            }
+
+            var result = data.RateArticle(this.User.Identity.GetUserId(), model.ArticleId, model.Rating);
+
+            if (result == ServiceResponse.Duplicated)
+            {
+                return this.BadRequest("You have already rated this article!");
+            }
+            
+            if (result == ServiceResponse.NotFound)
+            {
+                return this.BadRequest("The article could not be found!");
+            }
+
+            if (result == ServiceResponse.Own)
+            {
+                return this.BadRequest("You cannot rate your own article!");
             }
 
             return this.Ok();
---
+++ Server/Negwork.WebApi/Models/RatingModels/RatingCreationModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Negwork.WebApi.Models.RatingModels
+{
+    public class RatingCreationModel
+    {
+        [Required]
+        public int ArticleId { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public int Rating { get; set; }
+    }
+}
