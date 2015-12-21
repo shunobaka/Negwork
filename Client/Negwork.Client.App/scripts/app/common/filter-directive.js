dde03d833331fbff51e9ebd4272656e45477fe7e
shunobaka <vesselin_minev@abv.bg>
2015-12-21 10:50:07 +0200

    Extracted filter-panel into a directive

---
+++ Client/Negwork.Client.App/scripts/app/common/filter-directive.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    var filterPanelDirective = function filterPanelDirective() {
+        return {
+            restrict: 'A',
+            scope: false,
+            templateUrl: '../../partials/common/filter-panel.html'
+        };
+    };
+
+    angular
+        .module('negwork.directives')
+        .directive('filterPanel', [filterPanelDirective]);
+}());
