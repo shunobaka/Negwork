f9042c0d63cd1987efb518d3ef99fc5588ee6ff5
shunobaka <vesselin_minev@abv.bg>
2015-12-21 10:50:37 +0200

    Created display for articles

--- Client/Negwork.Client.App/scripts/app/articles/articles-controller.js
+++ Client/Negwork.Client.App/scripts/app/articles/articles-controller.js
@@ -1,13 +1,19 @@
 ﻿(function () {
     'use strict';
 
-    var articlesController = function articlesController(articles) {
+    var articlesController = function articlesController(notifier, articles) {
         var vm = this;
 
-        vm.articles = articles.all();
+        articles.all()
+            .then(function (response) {
+                vm.DefaultProfile = 'https://diasp.eu/assets/user/default.png';
+                vm.articles = response;
+            }, function (err) {
+                notifier.error(err);
+            });
     }
 
     angular
         .module('negwork.controllers')
-        .controller('ArticlesController', ['articles', articlesController])
+        .controller('ArticlesController', ['notifier', 'articles', articlesController])
 }());
