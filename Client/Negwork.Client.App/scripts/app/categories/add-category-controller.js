5b68e230ecab2fb2711b1640bde36102998442e0
shunobaka <vesselin_minev@abv.bg>
2015-12-25 00:19:05 +0200

    Implemented part of controller for creating categories

---
+++ Client/Negwork.Client.App/scripts/app/categories/add-category-controller.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    var addCategoryController = function addCategoryController($location, notifier, categories) {
+        var vm = this;
+
+        vm.create = function (category, form) {
+            if (form.$valid) {
+                categories.add(category)
+                    .then(function (response) {
+                        notifier.success('Category was successfully created!');
+                        $location.path('/categories');
+                    }, function (err) {
+                        notifier.error(err);
+                    })
+            }
+        }
+    }
+
+    angular
+        .module('negwork.controllers')
+        .controller('AddCategoryController', ['$location', 'notifier', 'categories', addCategoryController])
+}());
