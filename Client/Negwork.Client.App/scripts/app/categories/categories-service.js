f33d82c03b7c079dcc9b345a9d560d1de293dcfa
shunobaka <vesselin_minev@abv.bg>
2015-12-25 00:18:44 +0200

    Implemented categories service

---
+++ Client/Negwork.Client.App/scripts/app/categories/categories-service.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    var categoriesService = function categoriesService(data) {
+        var CATEGORIES_API_URL = '/api/categories';
+
+        function getAll() {
+            return data.get(CATEGORIES_API_URL);
+        }
+
+        function getById(id) {
+            return data.get(CATEGORIES_API_URL + '/' + id);
+        }
+
+        function getForArticles() {
+            return data.get(CATEGORIES_API_URL + '/articles');
+        }
+
+        function getForImages() {
+            return data.get(CATEGORIES_API_URL + '/images');
+        }
+
+        function add(category) {
+            return data.post(CATEGORIES_API_URL, category);
+        }
+
+        return {
+            getAll: getAll,
+            getById: getById,
+            getForArticles: getForArticles,
+            getForImages: getForImages,
+            add: add
+        };
+    }
+
+    angular
+        .module('negwork.services')
+        .factory('categories', ['data', categoriesService]);
+}());
