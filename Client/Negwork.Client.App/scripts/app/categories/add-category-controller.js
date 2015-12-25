b5ec812525fd1784a25640b82abc91dbdb6f43fe
shunobaka <vesselin_minev@abv.bg>
2015-12-25 10:35:05 +0200

    Implemented controller for adding category

--- Client/Negwork.Client.App/scripts/app/categories/add-category-controller.js
+++ Client/Negwork.Client.App/scripts/app/categories/add-category-controller.js
@@ -11,7 +11,17 @@
                         notifier.success('Category was successfully created!');
                         $location.path('/categories');
                     }, function (err) {
-                        notifier.error(err);
+                        if (err.data.ModelState !== undefined) {
+                            for (var prop in err.data.ModelState) {
+                                var errors = err.data.ModelState[prop];
+
+                                for (var error in errors) {
+                                    notifier.error(errors[error]);
+                                }
+                            }
+                        } else {
+                            notifier.error(err.data.Message);
+                        }
                     })
             }
         }
