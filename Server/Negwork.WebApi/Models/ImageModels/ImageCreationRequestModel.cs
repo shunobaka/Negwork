60cae21917e6204994aec3cc2fd5fdc51e79362f
shunobaka <vesselin_minev@abv.bg>
2015-12-26 21:51:47 +0200

    Added request and response models for images

---
+++ Server/Negwork.WebApi/Models/ImageModels/ImageCreationRequestModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Negwork.WebApi.Models.ImageModels
+{
+    using Negwork.Common.Constants;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImageCreationRequestModel
+    {
+        [Required]
+        [Url(ErrorMessage = ErrorMessages.IMAGE_URL_INVALID)]
+        public string Url { get; set; }
+
+        [Required]
+        [MinLength(ModelConstants.MIN_IMAGE_TITLE_LENGHT, 
+            ErrorMessage = ErrorMessages.IMAGE_TITLE_TOO_SHORT)]
+        [MaxLength(ModelConstants.MAX_IMAGE_TITLE_LENGTH, 
+            ErrorMessage = ErrorMessages.IMAGE_TITLE_TOO_LONG)]
+        public string Title { get; set; }
+
+        [MinLength(ModelConstants.MIN_IMAGE_DESCRIPTION_LENGTH,
+            ErrorMessage = ErrorMessages.IMAGE_DESCRIPTION_TOO_SHORT)]
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string OwnerId { get; set; }
+    }
+}
---
+++ Server/Negwork.WebApi/Models/ImageModels/ImageResponseModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Negwork.WebApi.Models.ImageModels
+{
+    using CategoryModels;
+    using Infrastructure.Mappings;
+    using Data.Models;
+    using System;
+
+    public class ImageResponseModel : IMapFrom<Image>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public CategoryResponseModel Category { get; set; }
+
+        public DateTime? DatePublished { get; set; }
+
+        public UserIdentityResponseModel Owner { get; set; }
+    }
+}
