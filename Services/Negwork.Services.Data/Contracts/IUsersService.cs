c10f70f6cbe8954936ec14c82cfe4e159167789c
shunobaka <vesselin_minev@abv.bg>
2015-12-26 22:15:09 +0200

    Implemented UsersService

---
+++ Services/Negwork.Services.Data/Contracts/IUsersService.cs
@@ -0,0 +1,12 @@
+﻿namespace Negwork.Services.Data.Contracts
+{
+    using Negwork.Data.Models;
+    using System.Linq;
+
+    public interface IUsersService
+    {
+        IQueryable<User> GetAll();
+
+        User GetById(string id);
+    }
+}
---
+++ Services/Negwork.Services.Data/UsersService.cs
@@ -0,0 +1,31 @@
+﻿namespace Negwork.Services.Data
+{
+    using System;
+    using System.Linq;
+    using Negwork.Data.Models;
+    using Negwork.Services.Data.Contracts;
+    using Negwork.Data.Repositories;
+
+    public class UsersService : IUsersService
+    {
+        private IRepository<User> users;
+
+        public UsersService(IRepository<User> users)
+        {
+            this.users = users;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return this.users.All();
+        }
+
+        public User GetById(string id)
+        {
+            return this.users
+                .All()
+                .Where(u => u.Id == id)
+                .FirstOrDefault();
+        }
+    }
+}
