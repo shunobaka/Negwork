d77859f0402524f1db0c716d1bb7a090e804662b
shunobaka <vesselin_minev@abv.bg>
2016-01-03 00:17:07 +0200

    Implemented comments service

---
+++ Client/Negwork.Client.App/scripts/app/comments/comments-service.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    var commentsService = function commentsService(data) {
+        var COMMENTS_API_URL = '/api/comments';
+
+        function add(comment) {
+            return data.post(COMMENTS_API_URL, comment);
+        }
+
+        return {
+            add: add
+        };
+    }
+
+    angular
+        .module('negwork.services')
+        .factory('comments', ['data', commentsService])
+}());
