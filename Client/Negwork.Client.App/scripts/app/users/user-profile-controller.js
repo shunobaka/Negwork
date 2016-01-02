30c81bc5f9f0c652655203b2d1a328df0684dac2
shunobaka <vesselin_minev@abv.bg>
2016-01-02 19:18:51 +0200

    Added profile edit button to user profile

--- Client/Negwork.Client.App/scripts/app/users/user-profile-controller.js
+++ Client/Negwork.Client.App/scripts/app/users/user-profile-controller.js
@@ -1,22 +1,25 @@
 ﻿(function () {
     'use strict';
 
-    var userProfileController = function userProfileController($routeParams, $location, notifier, users) {
+    var userProfileController = function userProfileController($routeParams, $location, notifier, auth, users) {
         var vm = this;
         var username = $routeParams.username;
         vm.DefaultProfile = 'https://diasp.eu/assets/user/default.png';
-
         users.getUserInfo(username)
             .then(function (response) {
-                debugger;
                 vm.user = response;
             }, function () {
                 $location.path('/home');
                 notifier.error('There was a problem retrieving info for the user.');
             })
+
+        auth.getIdentity()
+            .then(function (user) {
+                vm.currentUser = user;
+            })
     };
 
     angular
         .module('negwork.controllers')
-        .controller('UserProfileController', ['$routeParams', '$location', 'notifier', 'users', userProfileController]);
+        .controller('UserProfileController', ['$routeParams', '$location', 'notifier', 'auth', 'users', userProfileController]);
 }());
