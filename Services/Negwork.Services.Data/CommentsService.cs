9c0fa9270a0453408fe58b8d4dcbd9532e500a09
shunobaka <vesselin_minev@abv.bg>
2016-01-02 22:02:28 +0200

    Implemented comments service for api

---
+++ Services/Negwork.Services.Data/CommentsService.cs
@@ -0,0 +1,31 @@
+﻿namespace Negwork.Services.Data
+{
+    using System;
+    using Contracts;
+    using Negwork.Data.Models;
+    using Negwork.Data.Repositories;
+    public class CommentsService : ICommentsService
+    {
+        private IRepository<Comment> comments;
+
+        public CommentsService(IRepository<Comment> comments)
+        {
+            this.comments = comments;
+        }
+
+        public Comment CreateComment(string userId, string content, int articleId)
+        {
+            var comment = new Comment()
+            {
+                ArticleId = articleId,
+                UserId = userId,
+                Content = content
+            };
+
+            comments.Add(comment);
+            comments.SaveChanges();
+
+            return comment;
+        }
+    }
+}
---
+++ Services/Negwork.Services.Data/Contracts/ICommentsService.cs
@@ -0,0 +1,9 @@
+﻿namespace Negwork.Services.Data.Contracts
+{
+    using Negwork.Data.Models;
+
+    public interface ICommentsService
+    {
+        Comment CreateComment(string userId, string content, int articleId);
+    }
+}
