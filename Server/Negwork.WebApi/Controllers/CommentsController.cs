3f60c28ed28fadad42c77da4102d1190eb42fff3
shunobaka <vesselin_minev@abv.bg>
2016-01-02 22:08:44 +0200

    Created CommentsController with model for creating comments

---
+++ Server/Negwork.WebApi/Controllers/CommentsController.cs
@@ -0,0 +1,33 @@
+﻿namespace Negwork.WebApi.Controllers
+{
+    using Microsoft.AspNet.Identity;
+    using Models.CommentModels;
+    using Services.Data.Contracts;
+    using System.Web.Http;
+
+    public class CommentsController : ApiController
+    {
+        private ICommentsService data;
+
+        public CommentsController(ICommentsService data)
+        {
+            this.data = data;
+        }
+
+        [Authorize]
+        public IHttpActionResult Post(CommentCreationRequestModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var created = this.data.CreateComment(
+                this.User.Identity.GetUserId(),
+                model.Content,
+                model.ArticleId);
+
+            return this.Ok(created);
+        }
+    }
+}
---
+++ Server/Negwork.WebApi/Models/CommentModels/CommentCreationRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Negwork.WebApi.Models.CommentModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CommentCreationRequestModel
+    {
+        [Required]
+        public string Content { get; set; }
+        
+        [Required]
+        public int ArticleId { get; set; }
+    }
+}
