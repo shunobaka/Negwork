2ee5676a294deb7e6ce8df87c7a5d869f6a9a576
shunobaka <vesselin_minev@abv.bg>
2016-01-02 13:48:51 +0200

    Implemented user service and controller for user profiles

---
+++ Client/Negwork.Client.App/scripts/app/users/user-profile-controller.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    var userProfileController = function userProfileController($routeParams, $location, notifier, users) {
+        var vm = this;
+        var username = $routeParams.username;
+        vm.DefaultProfile = 'https://diasp.eu/assets/user/default.png';
+
+        users.getUserInfo(username)
+            .then(function (response) {
+                debugger;
+                vm.user = response;
+            }, function () {
+                $location.path('/home');
+                notifier.error('There was a problem retrieving info for the user.');
+            })
+    };
+
+    angular
+        .module('negwork.controllers')
+        .controller('UserProfileController', ['$routeParams', '$location', 'notifier', 'users', userProfileController]);
+}());
---
+++ Client/Negwork.Client.App/scripts/app/users/users-service.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    var usersService = function usersService(data) {
+        function getUserInfo(username) {
+            return data.get('/api/users/profile/' + username);
+        };
+
+        return {
+            getUserInfo: getUserInfo
+        };
+    }
+
+    angular
+        .module('negwork.services')
+        .factory('users', ['data', usersService]);
+}());
