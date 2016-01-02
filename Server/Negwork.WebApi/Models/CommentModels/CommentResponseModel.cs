2969d3471b89c1656fae3dcc725a8de095c68993
shunobaka <vesselin_minev@abv.bg>
2016-01-03 00:16:32 +0200

    Extended comment response model

--- Server/Negwork.WebApi/Models/CommentModels/CommentResponseModel.cs
+++ Server/Negwork.WebApi/Models/CommentModels/CommentResponseModel.cs
@@ -1,9 +1,16 @@
 ﻿namespace Negwork.WebApi.Models.CommentModels
 {
-    public class CommentResponseModel
+    using Data.Models;
+    using Negwork.WebApi.Infrastructure.Mappings;
+    using System;
+    public class CommentResponseModel : IMapFrom<Comment>
     {
         public int Id { get; set; }
 
         public string Content { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+
+        public UserIdentityResponseModel User { get; set; }
     }
 }
