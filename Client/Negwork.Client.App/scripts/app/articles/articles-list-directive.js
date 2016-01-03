1cdb0e41dd6e2411449e4a0f98b5496358a1ffb1
shunobaka <vesselin_minev@abv.bg>
2016-01-03 12:17:01 +0200

    Implemented articles list with directive for displaying table of articles

---
+++ Client/Negwork.Client.App/scripts/app/articles/articles-list-directive.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    'use strict';
+
+    var articlesListDirective = function articlesListDirective() {
+        return {
+            restrict: 'A',
+            scope: false,
+            templateUrl: '../../partials/articles/articles-list.html'
+        };
+    };
+
+    angular
+        .module('negwork.directives')
+        .directive('articlesList', [articlesListDirective]);
+}());
